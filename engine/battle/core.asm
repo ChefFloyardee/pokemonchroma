3d8f8f2ac035226db3387a17ed637ee2d7adeb42
Serhatisb <atahanisbakan@gmail.com>
2017-03-15 10:24:40 +0300

    Decapped stat names.

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7978,12 +7978,12 @@ PrintStatText:
 	jp CopyData
 
 StatsTextStrings:
-	db "ATTACK@"
-	db "DEFENSE@"
-	db "SPEED@"
-	db "SPECIAL@"
-	db "ACCURACY@"
-	db "EVADE@"
+	db "Attack@"
+	db "Defense@"
+	db "Speed@"
+	db "Special@"
+	db "Accuracy@"
+	db "Evade@"
 
 StatModifierRatios:
 ; first byte is numerator, second byte is denominator
