00a8be26b9af9d5de6f70fa0a31161dc2ff7c77c
Serhatisb <atahanisbakan@gmail.com>
2017-07-02 20:41:21 +0300

    Revert "Revert "Allow the Pokedex to have 251 Pokemon""
    This reverts commit bee1765de2804af807ccaaf8690cef7918de6e8c.

--- engine/menu/pokedex.asm
+++ engine/menu/pokedex.asm
@@ -195,7 +195,7 @@ HandlePokedexListMenu: ; 40111 (10:4111)
 	call PlaceString
 ; find the highest pokedex number among the pokemon the player has seen
 	ld hl,wPokedexSeenEnd - 1
-	ld b,(wPokedexSeenEnd - wPokedexSeen) * 8 + 1
+	ld b,255
 .maxSeenPokemonLoop
 	ld a,[hld]
 	ld c,8
