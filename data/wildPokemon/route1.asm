ea0f73c9cb33e36f5a437c67df136d0d93fd8235
Serhatisb <atahanisbakan@gmail.com>
2017-07-04 13:18:55 +0300

    Set wild encounters to test the 251 Pokemon

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,13 +1,13 @@
 Route1Mons:
 	db $19
-	db 3,PIDGEY
-	db 3,RATTATA
-	db 3,SENTRET
-	db 2,SENTRET
-	db 2,HOOTHOOT
-	db 3,HOOTHOOT
-	db 3,PIDGEY
-	db 5,TOTODILE
-	db 4,PIDGEY
-	db 5,RATTATA
+	db 3,CELEBI
+	db 3,HOOH
+	db 3,LUGIA
+	db 2,TYRANITAR
+	db 2,PUPITAR
+	db 3,LARVITAR
+	db 3,SUICUNE
+	db 5,ENTEI
+	db 4,RAIKOU
+	db 5,BLISSEY
 	db $00
